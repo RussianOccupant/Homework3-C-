ffd71b8786f0e3ffc5f3b8e41fb04d24876e95ee
AlexBystrov <sshbystrov@gmail.com>
2023-01-21 18:52:14 +0400

    Added answer for exersice 23

---
+++ Exersice23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу,которая принимает на вход число (N) и выдает таблицу кубов от 1 до N
+Console.Write("Enter number: ");
+int N = int.Parse(Console.ReadLine()!);
+int count = 1;
+Console.Write($"{N} -> ");
+while (count < N)
+{
+    Console.Write($"{Math.Pow(count, 3)}, ");
+    count++;
+}
+Console.Write($"{Math.Pow(count, 3)}.");
